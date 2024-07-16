7bb645da96792c95b6e0a458cf6593d6203c49f0
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-16 16:52:14 -0400

    Insert Bears into table

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 12, 'M', 'Dark Brown', 'Sweet but Melts in the Sun', true),
+('Rowdy', 8, 'M', 'Black', 'Always Ready to Party', true),
+('Tabitha', 6, 'F', 'Golden Brown', 'Sophisticated and Loves Tea Parties', true),
+('Sergeant Brown', 15, 'M', 'Brown', 'Strict but Fair', true),
+('Melissa', 5, 'F', 'Honey Yellow', 'Queen Bee of the Forest', true),
+('Grinch', 7, 'M', 'Greenish-Brown', 'Steals Picnics but Has a Heart of Gold', true),
+('Wendy', 9, 'F', 'White', 'Ice Queen with a Warm Heart', true),
+(NULL, 3, 'F', 'Camouflage', 'Invisible but Friendly', true);
